caf4c07d4532914cea1e80214e971e7e6a8a1f9c
Sarah Leong <sleong20@stuy.edu>
2021-01-06 13:13:36 -0500

    added get methods for instance variables

--- Node.java
+++ Node.java
@@ -1,10 +1,20 @@
 public class Node{
-public Node(String value) {
-    data = value;
-    next = null;
-    prev = null;
-}
-private String data;
-private Node next,prev;
-//write get/set methods for all three instance variables.
+    public Node(String value) {
+        data = value;
+        next = null;
+        prev = null;
+    }
+    private String data;
+    private Node next,prev;
+    //write get/set methods for all three instance variables.
+
+    public void setData(String value) {
+        data = value;
+    }
+    public void setNext(Node n) {
+        next = n;
+    }
+    public void setPrev(Node n) {
+        prev = n;
+    }
 }

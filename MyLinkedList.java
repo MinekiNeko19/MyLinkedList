4efe9ed0dcb31542d372e902863a3ee2337cab47
Sarah Leong <sleong20@stuy.edu>
2021-01-06 13:08:37 -0500

    added constructor

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,8 +1,14 @@
 public class MyLinkedList{
     private int size;
     private Node start,end;  
-    public MyLinkedList(){/*create a constructor*/}
-    public int size();
+    public MyLinkedList() {
+        size = 0;
+        start = null;
+        end = null;
+    }
+    public int size() {
+        return size;
+    };
     public boolean add(String value);
     public boolean add(int index, String value);
     public String get(int index);

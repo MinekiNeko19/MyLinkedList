e5d54439f455dcc38883bfeeff584f315507139a
Sarah Leong <sleong20@stuy.edu>
2021-01-06 22:41:09 -0500

    added a reset helper

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,3 +1,5 @@
+import java.lang.IndexOutOfBoundsException;
+
 public class MyLinkedList{
     private int size;
     private Node start,end;  
@@ -22,11 +24,13 @@ public class MyLinkedList{
         end = temp;
         return true;
     }
-    public boolean add(int index, String value) {
-        return true;
+    public void add(int index, String value) {
+        Node temp = nodeAtIndex(index);
+
     }
     public String get(int index) {
         return start.next().data();
+        // not correct
     }
     public String set(int index, String value) {
         return "";
@@ -36,11 +40,20 @@ public class MyLinkedList{
     }
     //Any helper method that returns a Node object MUST BE PRIVATE!
     private Node nodeAtIndex(int index) {
-        if (index < 1 || index > size()) return null;
+        if (index < 1 || index > size()) throw new IndexOutOfBoundsException();
         for (int i = 0; i < index; i++) {
             start=start.next();
         }
         return start;
+    }
+
+    private void reset() {
+        while (start.prev() != null) {
+            start = start.prev();
+        }
+        while (end.next() != null) {
+            end = end.next();
+        }
     }
 
     public static void main(String[] args) {
@@ -57,5 +70,14 @@ public class MyLinkedList{
 
         //testing helper method nodeAtIndex
         System.out.println(a.nodeAtIndex(3).data());
+
+        //testing helper method reset()
+        // a.reset();
+        // System.out.println(a.start.prev());
+        // System.out.println(a.end.next());
+
+
+        //testing add(index, value)
+
     }
    }

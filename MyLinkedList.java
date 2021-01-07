f53297da267b90f5acd3efc087f96691cadaf1c8
Sarah Leong <sleong20@stuy.edu>
2021-01-06 22:18:35 -0500

    added add(String value); fixed initializing end in constructor

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -4,15 +4,46 @@ public class MyLinkedList{
     public MyLinkedList() {
         size = 0;
         start = null;
-        end = start;
+        end = null;
     }
     public int size() {
         return size;
     };
-    public boolean add(String value);
-    public boolean add(int index, String value);
-    public String get(int index);
-    public String set(int index, String value);
-    public String toString();
+    public boolean add(String value) {
+        size++;
+        if (start == null) {
+            start = new Node(value);
+            end = start;
+            return true;
+        }
+        Node temp = new Node(value);
+        end.setNext(temp);
+        temp.setPrev(end);
+        end = temp;
+        return true;
+    }
+    public boolean add(int index, String value) {
+        return true;
+    }
+    public String get(int index) {
+        return start.next().data();
+    }
+    public String set(int index, String value) {
+        return "";
+    }
+    public String toString() {
+        return "";
+    }
     //Any helper method that returns a Node object MUST BE PRIVATE!
+
+    public static void main(String[] args) {
+        // Testing size and add(String value) GET(INT INDEX) CURRENTLY IS NOT CORRECT
+        MyLinkedList a = new MyLinkedList();
+        a.add("primodial soup");
+        System.out.println(a.size());
+        // System.out.println(a.get(0));
+        a.add("unicellular organism");
+        System.out.println(a.size());
+        System.out.println(a.get(0));
+    }
    }
